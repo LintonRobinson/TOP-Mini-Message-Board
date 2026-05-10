8c34da1709a135844a52bce93260a033568ccb62
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-05-09 21:54:28 -0400

    db: add error handling to all queries and export module
    - wrap all query functions in try/catch blocks
    - fix getGroomingOrder to query by id instead of dog name
    - fix insertGroomingOrder to use correct form field names
    - export all query functions from module

--- db/queries.js
+++ db/queries.js
@@ -1,19 +1,43 @@
 const pool = require("./pool.js");
 
 async function getGroomingOrders() {
-  const { rows } = await pool.query("SELECT * FROM grooming_orders ");
+  try {
+    const { rows } = await pool.query("SELECT * FROM grooming_orders;");
+  } catch (error) {
+    console.error("query error:", err);
+  }
+
   return rows;
 }
 
 async function getGroomingOrder(id) {
-  const { rows } = await pool.query("SELECT * FROM grooming_orders WHERE id = $1", [`%${dog}%`]);
-  return rows;
+  try {
+    const { rows } = await pool.query("SELECT * FROM grooming_orders WHERE id = $1", [id]);
+    return rows;
+  } catch (err) {
+    console.error("query error:", err);
+  }
 }
 
 async function insertGroomingOrder(groomingOrder) {
-  await pool.query("INSERT INTO grooming_orders (dog,grooming_request,added) VALUES ($1,$2,$3)", [groomingOrder.dog, groomingOrder.groomingRequest, groomingOrder.added]);
+  try {
+    await pool.query("INSERT INTO grooming_orders (dog,grooming_request,added) VALUES ($1,$2,$3)", [groomingOrder.dog_name, groomingOrder.grooming_requests, groomingOrder.added]);
+  } catch (error) {
+    console.error("query error:", err);
+  }
 }
 
 async function deleteGroomingOrder(id) {
-  await pool.query("DELETE FROM grooming_orders WHERE dog LIKE $1", [`%${dog}%`]);
+  try {
+    await pool.query("DELETE FROM grooming_orders WHERE dog LIKE $1", [`%${dog}%;`]);
+  } catch (error) {
+    console.error("query error:", err);
+  }
 }
+
+module.exports = {
+  getGroomingOrders,
+  getGroomingOrder,
+  insertGroomingOrder,
+  deleteGroomingOrder,
+};
